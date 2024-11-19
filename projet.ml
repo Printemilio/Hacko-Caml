61a9f80b4d2f4edabb1a9756f8849c9732038183
Slepsni <163827750+Slepsni@users.noreply.github.com>
2024-11-19 11:59:55 +0100

    Update projet.ml

--- projet.ml
+++ projet.ml
@@ -21,3 +21,27 @@ let read_data_from_file file =
 let hash_password pwd =
   Base64.encode_exn(Cryptokit.hash_string (Cryptokit.Hash.sha256 ()) pwd)
 ;;
+
+
+let fusinfo ( file1,file2 : string * string): (string * string) list =
+  let listefil1 : (string * string) list = read_data_from_file file1 
+  and listefil2 : (string * string) list = read_data_from_file file2 in
+  let listconcaten : (string * string) list = listefil1 @ listefil2 in 
+  let rec aux (listdep, listfin : (string * string) list *(string * string) list) : (string * string) list =
+    if listdep = [] 
+    then listfin
+    else 
+      (
+        let listtmp : (string * string) list ref = ref listfin 
+        and ispresent : bool = false in
+        while !listtmp <> [] do 
+          if List.hd listdep = List.hd !listtmp
+          then ispresent:= true ;
+          listtmp :=  List.tl !listtmp
+        done;
+        if not (ispresent) 
+        then aux (List.tl listdep, (List.hd listdep) :: listfin)
+        else aux (List.tl listdep,  listfin)
+      )
+  in aux (listconcaten,[])
+;;
